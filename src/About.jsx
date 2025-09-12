ce903f3def2006b9e1b3c050f0b081b5bf16c2f9
Rohit Goparaju <rohitgoparaju.r96@gmail.com>
2025-09-12 21:07:24 +0530

    update about page

--- src/About.jsx
+++ src/About.jsx
@@ -1,16 +1,54 @@
 export default function About(){
     return (
         <div className="text-bg-secondary m-5 p-5 rounded shadow">
-            <h1>About page</h1>
-            <p>
-                This is a java full stack project build using java, react, spring boot, html, css, javascript and various 
-                other java technologies.
-            </p>
-            <h1>Credits and attributions:</h1>
-            <ul>
-                <li>Logo by <a href="https://www.svgrepo.com" className="text-light" target="_blank" rel="noopener noreferrer">SVG Repo</a></li>
-                <li>Avatar image by <a href="http://www.freepik.com" className="text-light" target="_blank" rel="noopener noreferrer">studiogstock / Freepik</a></li>
-            </ul>
+            <div>
+                <h1>About page</h1>
+                <p>
+                    This is a java full stack project built by <b><i>Rohit Goparaju</i></b>.
+                </p>
+            </div>
+            <div>
+                <h1>Technologies used:</h1>
+                <ul>
+                    <li>
+                        Java
+                    </li>
+                    <li>
+                        React
+                    </li>
+                    <li>
+                        HTML
+                    </li>
+                    <li>
+                        CSS
+                    </li>
+                    <li>
+                        Javascript
+                    </li>
+                    <li>
+                        Bootstrap
+                    </li>
+                    <li>
+                        Git
+                    </li>
+                    <li>
+                        Github
+                    </li>
+                    <li>
+                        Spring boot
+                    </li>
+                    <li>
+                        SQL
+                    </li>
+                </ul>
+            </div>
+            <div>    
+                <h1>Credits and attributions:</h1>
+                <ul>
+                    <li>Logo by <a href="https://www.svgrepo.com" className="text-light" target="_blank" rel="noopener noreferrer">SVG Repo</a></li>
+                    <li>Avatar image by <a href="http://www.freepik.com" className="text-light" target="_blank" rel="noopener noreferrer">studiogstock / Freepik</a></li>
+                </ul>
+            </div>
         </div>
     );
 }
